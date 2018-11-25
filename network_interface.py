6ff373044984de35e27be5710fd61c0e5987feab
bartTam <mrbart.tam@gmail.com>
2018-11-24 16:27:08 -0800

    Added network interface

---
+++ network_interface.py
@@ -0,0 +1,26 @@
+import socket
+
+class Connection():
+
+    @staticmethod
+    def create_server(addr=socket.gethostname(), port=5678):
+        conn = socket.socket()
+        conn.bind((addr, port))
+        conn.listen()
+        return Connection(conn)
+
+    @staticmethod
+    def create_client(addr="127.0.0.1", port=5678):
+        conn = socket.socket()
+        conn.connect((addr, port))
+        return Connection(conn)
+
+    def __init__(self, connection):
+        self.conn = connection
+
+    def update(self, gamestate):
+        self.conn.send(gamestate)
+        return self.conn.recv(4096)
+
+    def close(self):
+        self.conn.close()
