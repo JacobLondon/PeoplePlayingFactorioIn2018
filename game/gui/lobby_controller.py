a7922c50c12981d2e02d1a58ae8a004e734aea59
Jacob <jelondon12@gmail.com>
2019-01-07 20:33:12 -0800

    textboxes are working

--- game/gui/lobby_controller.py
+++ game/gui/lobby_controller.py
@@ -80,7 +80,6 @@ class Lobby_Controller(Controller):
             self.join_button_clicked()
         elif self.back_button.focused:
             self.back_button_clicked()
-
         elif self.ip_textbox.focused:
             self.ip_textbox.typing = True
         elif self.test_textbox.focused:
